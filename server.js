997fdc9a792254f94f091a290fc5a0939da5795d
grantwbrooks <grantwbrooks@gmail.com>
2017-10-06 14:09:57 -0400

    finished

---
+++ server.js
@@ -0,0 +1,30 @@
+// Import express and path modules.
+var express = require( "express");
+var path = require( "path");
+// Create the express app.
+var app = express();
+// Define the static folder.
+app.use(express.static(path.join(__dirname, "./static")));
+// Setup ejs templating and define the views folder.
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// Root route to render the index.ejs view.
+app.get('/', function(req, res) {
+ res.render("index");
+})
+// Start Node server listening on port 8000, create server var and put it in listen method
+var server = app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log("Client/socket is connected!");
+    console.log("Client/socket id is: ", socket.id);
+    // all the server socket code goes in here
+    socket.on("posting_form", function (data){
+        let rando = Math.floor(Math.random()*1000)+1
+        socket.emit('updated_message', {response: data, number: rando});
+        // socket.emit('random_number', {response: Math.floor(Math.random() * 1000)+1});
+    })
+})
